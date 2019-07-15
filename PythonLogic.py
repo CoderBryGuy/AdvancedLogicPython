be1d6fcace9a1ca429ad92f6bf48ca325828b264
Bryan <bryan.dov.bergman@gmail.com>
2019-07-15 18:04:01 +0300

    Initial commit

---
+++ PythonLogic.py
@@ -0,0 +1,80 @@
+_author_ = 'dev'
+
+# name = input("please enter your name: ")
+# age = int(input("How old are you"))
+# print(age)
+#
+# if age >=18:
+#     print("you are old enough to vote")
+#     print("Please put an X in the box")
+# else:
+#     print("please come back in {0} years".format(18-age))
+# print("Please guess a number between 1 an 10: ")
+# guess = int(input())
+#
+# if guess != 5:
+#     if guess <5:
+#         print("Please guess higher")
+#     else:
+#         print("Please guess lower")
+#
+#         guess = int(input())
+#         if guess == 5:
+#             print("well done you guessed it")
+#         else:
+#             print("Sorry, you have not guessed correctly")
+# else:
+#     print("You guess it the first time")
+
+# if (age >= 16) and (age <= 65):
+# if 16 <= age <= 65:
+#     print("have a good day at work")
+#
+# if age < 16 or age > 65:
+#     print("enjoy your free time")
+# else:
+#     print("have a good day at work")
+#
+# x = "false"
+# if x:
+#     print("x is true:")
+
+# x = input("please enter some text")
+#
+# if x:
+#     print("You entered '{}'".format(x))
+# else:
+#     print("You did not enter anything")
+#
+
+# print(not False)
+# print(not True)
+
+# age = int(input("How old are you?"))
+#
+# if not(age < 18):
+#     print("You are old enough to vote")
+#     print("Please put an X in the box")
+# else:
+#     print("please come back in {0} years".format(18 - age))
+
+
+# parrot = "Norwegian blue"
+# letter = input("enter a character:")
+#
+# if letter not in parrot:
+#     print("I don't need that letter")
+#
+# else:
+#     print("Give me an {}, Bob".format(letter))
+
+x = 5
+y = 7
+
+if x > y:
+    print("x is greater than y")
+elif x == y:
+    print("x equals y")
+else:
+    print("x is smaller than y")
+
